30dc88bbe3c5cf6b25e2b4ee2edbcdd742a9c601
timurbey <atpoverload@gmail.com>
2021-01-11 10:08:35 -0700

    trying a singleton wrapper for Rapl.java

--- src/java/jrapl/Rapl.java
+++ src/java/jrapl/Rapl.java
@@ -6,59 +6,42 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import java.io.File;
+import java.util.Arrays;
+
 /** Class that provides a static interface to RAPL measurements. */
-public class Rapl {
-  private static native int ProfileInit();
+public final class Rapl {
+  private static Rapl instance;
 
-  private static native int GetSocketNum();
-
-  private static native String EnergyStatCheck();
-
-  // adapted from https://github.com/adamheinrich/native-utils
-  private static void loadFromJar(String library) throws IOException {
-    File temp = File.createTempFile(library, null);
-    temp.deleteOnExit();
-
-    if (!temp.exists()) {
-      throw new FileNotFoundException("Could not create a temporary file.");
-    }
-
-    // Prepare buffer for data copying
-    byte[] buffer = new byte[1024];
-    int readBytes;
-
-    try (InputStream is = Rapl.class.getResourceAsStream(library)) {
-      if (is == null) {
-        throw new FileNotFoundException("Could not find library " + library + " in jar.");
-      }
-      // Open output stream and copy data between source file in JAR and the temporary file
-      try (FileOutputStream os = new FileOutputStream(temp)) {
-        try {
-          while ((readBytes = is.read(buffer)) != -1) {
-            os.write(buffer, 0, readBytes);
-          }
-        } finally {
-          System.load(temp.getAbsolutePath());
-        }
-      }
-    }
+  public static Rapl getInstance() {
+    return getInstance(null);
   }
 
-  public static int SOCKET_COUNT = -1;
-  // value to add to energy diffs after underflow
-  public static double WRAP_AROUND_ENERGY = 0;
-
-  private static Object lock = new Object();
-
-  public static void loadRapl(String path) {
-    synchronized (lock) {
-      if (SOCKET_COUNT < 0) {
-        System.load(path);
-        // loadFromJar("/jrapl/libCPUScaler.so");
-        WRAP_AROUND_ENERGY = ProfileInit();
-        SOCKET_COUNT = GetSocketNum();
-      }
+  public static synchronized Rapl getInstance(String path) {
+    if (instance != null) {
+      return instance;
     }
+
+    if (path == null) {
+      System.loadLibrary("CPUScaler");
+    } else {
+      System.load(path);
+    }
+
+    instance = new Rapl();
+    return instance;
+  }
+
+  private native int ProfileInit();
+  private native int GetSocketNum();
+  private native String EnergyStatCheck();
+
+  private final int socketCount;
+  private final double wrapAroundEnergy;
+
+  private Rapl() {
+    wrapAroundEnergy = ProfileInit();
+    socketCount = GetSocketNum();
   }
 
   /**
@@ -66,13 +49,13 @@ public class Rapl {
    *     <p>subarray structure is architecture dependent. Typically, 0 -> dram, 1 -> cpu, 2 ->
    *     package.
    */
-  public static double[][] getEnergyStats() {
+  public double[][] getEnergyStats() {
     // guard if CPUScaler isn't available
-    if (SOCKET_COUNT < 0) {
+    if (socketCount < 0) {
       return new double[0][0];
     }
     String EnergyInfo = EnergyStatCheck();
-    if (SOCKET_COUNT == 1) {
+    if (socketCount == 1) {
       /*One Socket*/
       double[][] stats = new double[1][3];
       String[] energy = EnergyInfo.split("#");
@@ -85,7 +68,7 @@ public class Rapl {
     } else {
       /*Multiple sockets*/
       String[] perSockEner = EnergyInfo.split("@");
-      double[][] stats = new double[SOCKET_COUNT][3];
+      double[][] stats = new double[socketCount][3];
       int count = 0;
 
       for (int i = 0; i < perSockEner.length; i++) {
@@ -99,10 +82,16 @@ public class Rapl {
     }
   }
 
-  private Rapl() {}
+  public int getSocketCount() {
+    return socketCount;
+  }
+
+  public double getWrapAroundEnergy() {
+    return wrapAroundEnergy;
+  }
 
   public static void main(String[] args) {
-    for (double[] socketEnergy : getEnergyStats()) {
+    for (double[] socketEnergy : getInstance("/src/jrapl/libCPUScaler.so").getEnergyStats()) {
       for (double componentEnergy : socketEnergy) {
         System.out.println(componentEnergy);
       }
